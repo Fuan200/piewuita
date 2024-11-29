715a50bc6dc3ab58833c4b0639402b4d6c82822a
Fuan200 <juandiazfdez1992@gmail.com>
2024-11-29 15:23:51 -0600

    update license

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url='https://github.com/Fuan200/',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
     entry_points={
