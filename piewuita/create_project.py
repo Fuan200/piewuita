b024882d5e18635466c0c373d447225ef5d61eb4
Fuan200 <juandiazfdez1992@gmail.com>
2025-06-15 10:30:04 -0600

    remove pylint

--- piewuita/create_project.py
+++ piewuita/create_project.py
@@ -25,20 +25,20 @@ def create_project(project_name, modules):
 
     venv_path = os.path.join(project_name, "venv")
     pip_executable = os.path.join(venv_path, "bin", "pip")
-    pylint_executable = os.path.join(venv_path, "bin", "pylint")
+    #pylint_executable = os.path.join(venv_path, "bin", "pylint")
 
     try:
         # Create virtual environment and install modules
         subprocess.run(["python3", "-m", "venv", venv_path], check=True)
-        base_modules = ["pylint", "black"]
+        base_modules = ["black"] # "pylint"
         modules = base_modules + modules if modules else base_modules
         subprocess.run([pip_executable, "install"] + modules, check=True)
         subprocess.run(f"{pip_executable} freeze > {project_name}/requirements.txt", shell=True, check=True)
 
         # Generate .pylintrc using pylint from the virtual environment
-        pylintrc_path = os.path.join(project_name, ".pylintrc")
-        with open(pylintrc_path, 'w') as pylintrc_file:
-            subprocess.run([pylint_executable, "--generate-rcfile"], stdout=pylintrc_file, check=True)
+        # pylintrc_path = os.path.join(project_name, ".pylintrc")
+        # with open(pylintrc_path, 'w') as pylintrc_file:
+        #     subprocess.run([pylint_executable, "--generate-rcfile"], stdout=pylintrc_file, check=True)
 
     except subprocess.CalledProcessError as e:
         print(f"Error setting up the environment: {e}")
